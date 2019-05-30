22012a5fd4ef64aae975df24a48acd00d485fa7c
Jorge Marquez <jmarquez@vendorpro.cl>
2019-05-30 17:14:02 -0400

    Ultimos cambios hasta la fecha

--- src/main/java/cl/duoc/app/controller/PeliculasController.java
+++ src/main/java/cl/duoc/app/controller/PeliculasController.java
@@ -1,9 +1,16 @@
 package cl.duoc.app.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,6 +38,13 @@ public class PeliculasController {
 		
 		System.out.println("Recibiendo objeto pelicula : " + pelicula);
 		return "peliculas/formPelicula";
+	}
+	
+	@InitBinder
+	public void initbinder(WebDataBinder binder) {
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
 	}
 
 }
